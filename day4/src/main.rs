dcbc4eabb5ba87173b5924ef553e51bf16169b65
Thompson Lane <thompsonplane@gmail.com>
2022-02-13 22:10:00 -0600

    Finished Day 4.

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -43,158 +43,238 @@ fn main() {
     }
 }
 
+struct BingoBoard {
+    numbers: [[i32; 5]; 5],
+    marked: [[bool; 5]; 5],
+    bingo: bool
+}
 
+impl BingoBoard {
+    fn new() -> Self {
+        Self {
+            numbers: [[0; 5]; 5],
+            marked: [[false; 5]; 5],
+            bingo: false,
+        }
+    }
+
+    fn clear(&mut self) {
+        self.numbers = [[0; 5]; 5];
+        self.marked = [[false; 5]; 5];
+        self.bingo = false;
+    }
+
+    fn empty(&self) -> bool {
+        for i in 0..self.numbers.len() {
+            for j in 0..self.numbers[i].len() {
+                if self.numbers[i][j] > 0 {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    fn contains(&self, value: i32) -> bool {
+        for i in 0..self.numbers.len() {
+            for j in 0..self.numbers[i].len() {
+                if self.numbers[i][j] == value {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    fn marked(&self, row: usize, col: usize) -> bool {
+        return self.marked[row][col];
+    }
+
+    fn mark(&mut self, value: i32) {
+        for i in 0..self.numbers.len() {
+            for j in 0..self.numbers[i].len() {
+                if self.numbers[i][j] == value {
+                    self.marked[i][j] = true;
+                }
+            }
+        }
+    }
+
+    fn check_bingo(&mut self) -> bool {
+        let row_length = self.marked.len();
+        let col_length = self.marked[0].len();
+
+        // Check each row
+        for i in 0..row_length {
+            let mut win = true;
+            for j in 0..col_length {
+                if !self.marked[i][j] {
+                    win = false;
+                    break;
+                }
+            }
+
+            if win {
+                self.bingo = true;
+                return true;
+            }
+        }
+
+        // Check each column
+        for i in 0..col_length {
+            let mut win = true;
+            for j in 0..row_length {
+                if !self.marked[j][i] {
+                    win = false;
+                    break;
+                }
+            }
+
+            if win {
+                self.bingo = true;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn is_bingo(&self) -> bool {
+        self.bingo
+    }
+
+    fn sum_unmarked(&self) -> i32 {
+        let mut sum = 0;
+        for i in 0..self.marked.len() {
+            for j in 0..self.marked[i].len() {
+                if !self.marked[i][j] {
+                    sum += self.numbers[i][j];
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+fn parse_input(input: &mut BufReader<File>) -> (Vec::<i32>, Vec::<BingoBoard>) {
+    let bingo_numbers_str = input.lines().nth(0).unwrap().unwrap();
+
+    let bingo_numbers_str = bingo_numbers_str.split(',');
+    let mut bingo_numbers = Vec::<i32>::new();
+    for number in bingo_numbers_str {
+        bingo_numbers.push(number.parse::<i32>().unwrap());
+    }
+
+    let mut bingo_boards = Vec::<BingoBoard>::new();
+    let mut bingo_board = BingoBoard::new();
+    let mut row = 0;
+    for line in input.lines() {
+        let line = line.unwrap();
+        if line.is_empty() {
+            if row > 0 {
+                bingo_boards.push(bingo_board);
+                bingo_board = BingoBoard::new();
+            }
+
+            row = 0;
+            continue;
+        }
+
+        let row_data = line.split(' ');
+        let mut column = 0;
+        for c in row_data {
+            let num = c.trim();
+            if num.len() > 0 {
+                let num = num.parse::<i32>().unwrap();
+                bingo_board.numbers[row][column] = num;
+                column += 1;
+            }
+        }
+
+        row += 1;
+    }
+
+    if !bingo_board.empty() {
+        bingo_boards.push(bingo_board);
+    }
+
+    print_boards(&bingo_boards);
+
+    return (bingo_numbers, bingo_boards);
+}
+
+fn print_board(board: &BingoBoard) {
+    for i in 0..board.numbers.len() {
+        for j in 0..board.numbers[i].len() {
+            print!("{number:>width$}", number=board.numbers[i][j], width=3);
+        }
+        println!("");
+    }
+}
+
+fn print_boards(boards: &Vec<BingoBoard>) {
+    for board in boards {
+        print_board(board);
+        println!("");
+    }
+}
 
 fn part1(input: &mut BufReader<File>) -> i32 {
-    let mut gamma_rate = 0;
-    let mut epsilon_rate = 0;
-    let mut counts = Vec::<i32>::new();
+    let (mut numbers, mut boards) = parse_input(input);
 
-    for line in input.lines() {
-        let number = line.unwrap();
-        let num_bits = number.len();
-
-        if counts.len() < num_bits {
-            counts.resize(num_bits, 0);
+    println!("{:?}", numbers);
+    for number in numbers {
+        for board in &mut boards {
+            board.mark(number);
+            //println!("Marked {}", number);
         }
 
-        for (i, c) in number.chars().enumerate() {
-            let bit = c.to_digit(2).unwrap();
-            counts[i] += if bit > 0 { 1 } else { -1 };
+        if number == 24 {
+            print_boards(&boards);
         }
-    }
 
-    for (i, count) in counts.iter().enumerate() {
-        if *count > 0 {
-            gamma_rate += 2i32.pow((counts.len() - i - 1) as u32);
-        }
-        else {
-            epsilon_rate += 2i32.pow((counts.len() - i - 1) as u32);
+        for board in &mut boards {
+            if board.check_bingo() {
+                println!("{} * {}", board.sum_unmarked(), number);
+                return board.sum_unmarked() * number;
+            }
         }
     }
 
-    return gamma_rate * epsilon_rate;
+    return 0;
 }
 
 fn part2(input: &mut BufReader<File>) -> i32 {
-    let mut oxygen_rating = 0;
-    let mut co2_rating = 0;
+    let (mut numbers, mut boards) = parse_input(input);
 
-    let mut numbers = Vec::<String>::new();
-    for line in input.lines() {
-        let number = line.unwrap();
-        numbers.push(number);
-    }
-
-    let oxygen_rating = calculate_oxygen_rating(&numbers);
-    let co2_rating = calculate_co2_rating(&numbers);
-
-    println!("oxygen rating: {}", oxygen_rating);
-    println!("co2 rating: {}", co2_rating);
-
-    return oxygen_rating * co2_rating;
-}
-
-fn calculate_oxygen_rating(input: &Vec<String>) -> i32 {
-    let mut numbers = input.clone();
-    let num_bits = numbers[0].len();
-
-    for i in 0..num_bits {
-        let mut count = 0;
-
-        if numbers.len() == 1 {
-            break;
+    println!("{:?}", numbers);
+    for number in numbers {
+        for board in &mut boards {
+            board.mark(number);
         }
 
-        //let mut remove = Vec::<usize>::new();
-        for (j, number) in numbers.iter().enumerate() {
-            let bit_b = number.as_bytes()[i];
-            let bit_c = bit_b as char;
-            let bit = bit_c.to_digit(2).unwrap();
+        let mut last_board_index = 0;
+        for (i, board) in &mut boards.iter_mut().enumerate() {
+            if !board.is_bingo() {
+                board.check_bingo();
 
-            if bit != 1 {
-                //remove.push(i);
-                count -= 1;
-            }
-            else {
-                count += 1;
+                if board.is_bingo() {
+                    last_board_index = i;
+                }
             }
         }
 
-        if count >= 0 {
-            // Keep only numbers with bit set to 1
-            numbers.drain_filter(|num| {
-                let bit = (num.as_bytes()[i] as char).to_digit(2).unwrap();
-                if bit == 0 {
-                    return true;
-                }
-                return false;
-            });
-        }
-        else {
-            // Keep only numbers with bit set to 0
-            numbers.drain_filter(|num| {
-                let bit = (num.as_bytes()[i] as char).to_digit(2).unwrap();
-                if bit == 1 {
-                    return true;
-                }
-                return false;
-            });
+        if boards.iter().all(|board| board.is_bingo()) {
+            let last_board = &boards[last_board_index];
+            println!("{}", number);
+            println!("{}", last_board_index);
+            return last_board.sum_unmarked() * number;
         }
     }
 
-    let num = i32::from_str_radix(numbers[0].as_str(), 2).unwrap();
-    return num;
+    return 0;
 }
 
-fn calculate_co2_rating(input: &Vec<String>) -> i32 {
-    let mut numbers = input.clone();
-    let num_bits = numbers[0].len();
-
-    for i in 0..num_bits {
-        let mut count = 0;
-
-        if numbers.len() == 1 {
-            break;
-        }
-
-        //let mut remove = Vec::<usize>::new();
-        for (j, number) in numbers.iter().enumerate() {
-            let bit_b = number.as_bytes()[i];
-            let bit_c = bit_b as char;
-            let bit = bit_c.to_digit(2).unwrap();
-
-            if bit != 1 {
-                //remove.push(i);
-                count -= 1;
-            }
-            else {
-                count += 1;
-            }
-        }
-
-        if count >= 0 {
-            // Keep only numbers with bit set to 0
-            numbers.drain_filter(|num| {
-                let bit = (num.as_bytes()[i] as char).to_digit(2).unwrap();
-                if bit == 1 {
-                    return true;
-                }
-                return false;
-            });
-        }
-        else {
-            // Keep only numbers with bit set to 1
-            numbers.drain_filter(|num| {
-                let bit = (num.as_bytes()[i] as char).to_digit(2).unwrap();
-                if bit == 0 {
-                    return true;
-                }
-                return false;
-            });
-        }
-    }
-
-    let num = i32::from_str_radix(numbers[0].as_str(), 2).unwrap();
-    return num;
-}
